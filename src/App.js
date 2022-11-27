c9218fbf38e0e55566d8211d1a98c75358d04ff5
bmalkes <bmalkes@gmail.com>
2022-11-27 09:20:46 +0200

    create router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "Pages/HomePage";
+import LoginPage from "Pages/LoginPage";
+import ProfilePage from "Pages/ProfilePage";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
