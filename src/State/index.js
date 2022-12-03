91281073b158e1e43e079f1051b581b68a8158f5
bmalkes <bmalkes@gmail.com>
2022-12-03 15:53:42 +0200

    fix:state setPost

--- src/State/index.js
+++ src/State/index.js
@@ -32,7 +32,7 @@ export const authSlice = createSlice({
     setPosts: (state, action) => {
       state.posts = action.payload.posts;
     },
-    setPosts: (state, action) => {
+    setPost: (state, action) => {
       const updatedPosts = state.posts.map((post) => {
         if (post._id === action.payload.post._id) return action.payload.post;
         return post;
